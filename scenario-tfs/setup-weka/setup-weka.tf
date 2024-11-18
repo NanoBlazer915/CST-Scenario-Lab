39bf467f6c681ea9ad2fabd256cb027b6987eeed
Jake <jacob.warners@weka.io>
2024-11-18 11:37:18 -0800

    put the tf's in the repo cause that makes sense

---
+++ scenario-tfs/setup-weka/setup-weka.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "us-east-1" 
+  }
+
+module "base_infrastructure" {
+  source      = "git::https://github.com/NanoBlazer915/CST-Scenario-Lab.git//modules/base"
+  name_prefix = "Setup-Weka"
+}
+
+module "scenario_infrastructure" {
+  source      = "git::https://github.com/NanoBlazer915/CST-Scenario-Lab.git//modules/setup-weka"
+  name_prefix = "Setup-Weka"
+
+
+  subnet_id         = module.base_infrastructure.subnet_id
+  private_subnet_id = module.base_infrastructure.private_subnet_id
+  security_group_id = module.base_infrastructure.security_group_id
+  key_name          = module.base_infrastructure.keypair_name
+  random_pet_id     = module.base_infrastructure.random_pet_id
+  private_key_pem = module.base_infrastructure.private_key_pem
+  other_private_ips = module.base_infrastructure.instance_private_ips
+  other_public_ips  = module.base_infrastructure.instance_public_ips
+}

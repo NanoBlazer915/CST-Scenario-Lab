5b69a1893d333bb2a0dd448016155bc980fe1a86
Jake <jacob.warners@weka.io>
2024-11-26 20:56:13 -0800

    miss named the variable in modules testing again

--- modules/secret-agent-man/scenario-ec2.tf
+++ modules/secret-agent-man/scenario-ec2.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "cst_scenario_specialty" {
   key_name                    = var.key_name
 
   # Attach the IAM instance profile
-  iam_instance_profile = var.aws_iam_instance_profile
+  iam_instance_profile = var.iam_instance_profile_name
 
 
   # Use the template file for user data

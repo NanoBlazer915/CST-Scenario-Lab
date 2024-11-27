d3221556d6b3550d8c8dee19e81a3d2cd3b89ed1
Jake <jacob.warners@weka.io>
2024-11-26 22:02:29 -0800

    maybe that

--- modules/dual-backend-failure/iam.tf
+++ modules/dual-backend-failure/iam.tf
@@ -1,3 +1,21 @@
+# Create an IAM role with a unique name
+resource "aws_iam_role" "ec2_instance_role_special" {
+  name = "ec2-instance-role-special-${var.random_pet_id}" # Use var.random_pet_id to make it unique
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
 # Create an IAM policy for describing and stopping instances, and accessing S3
 resource "aws_iam_policy" "special_instance_policy" {
   name        = "special-instance-policy-${var.random_pet_id}"

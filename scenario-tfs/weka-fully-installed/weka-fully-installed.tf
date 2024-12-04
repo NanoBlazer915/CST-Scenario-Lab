43d898f8b3f5d9782a2b331741f746c1506dfe83
Jake <jacob.warners@weka.io>
2024-12-03 18:10:43 -0800

    fixed name in weka full in stalled

--- scenario-tfs/weka-fully-installed/weka-fully-installed.tf
+++ scenario-tfs/weka-fully-installed/weka-fully-installed.tf
@@ -1,6 +1,6 @@
 module "base_infrastructure" {
   source      = "git::https://github.com/NanoBlazer915/CST-Scenario-Lab.git//modules/base"
-  name_prefix = "Change-bases"
+  name_prefix = "Basic-Weka"
   chaos_applied = false 
 
 }
